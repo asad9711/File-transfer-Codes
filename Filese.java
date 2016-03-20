fbdd34f51138094b18af281023c4d4f4caf66255
Mohd Asad Hussain <asadhussain9711@gmail.com>
2016-03-20 13:40:17 +0530

    Update Filese.java
    For sending file from one section of hard drive to another on the same system.

--- Filese.java
+++ Filese.java
@@ -12,18 +12,13 @@ class Filese
 
 		FileReader fr=new FileReader("data.txt");
 		BufferedReader br=new BufferedReader(fr);
-         String str=" ";
-		// do{
-  //              str=br.readLine();
-  //              dos.writeUTF(str);
-  //              dos.flush();
-		// }while(!str.equals(null));
-
-         while(!str.equals(null))
+         String str=null;
+	
+         while(!(str=br.readLine()).equals(null))
          {
-         	str=br.readLine();
+         	// str=br.readLine();
          	dos.writeUTF(str);
-         	str=br.readLine();
+         	// str=br.readLine();
          	dos.flush();
          }
 	}catch(Exception e)
